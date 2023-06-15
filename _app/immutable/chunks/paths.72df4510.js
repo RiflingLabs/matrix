55d0664e0a3bab7ccf9983d692676c1efb97b20c
Jacob Young <jacob@quantenverse.de>
2023-06-16 00:44:03 +0800

    Updates

---
+++ _app/immutable/chunks/paths.72df4510.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1rf8j1y)==null?void 0:s.base)??"/sveltekit-gh-pages";var e;const a=((e=globalThis.__sveltekit_1rf8j1y)==null?void 0:e.assets)??t;export{a,t as b};
